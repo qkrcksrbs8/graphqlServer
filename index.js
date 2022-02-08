42420ad823b9de2d649b9156e5410a7c1f490884
changyun <qkrcksrbs8@naver.com>
2022-02-07 20:12:06 -0800

    [NODE.JS] Graphql 테스트

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+// import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+
+// const books = [
+//     {title: '자바 메모리구조', author: '김땡땡'}
+//     ,{title: '디자인패턴', author: '박땡땡'}
+//     ,{title: 'v8엔진', author: '오땡땡'}
+// ]
+
+// const typeDefs =`
+//     type Query {
+//         books : [Book]
+//     }
+//     type Book {
+//         title:String
+//         author:String
+//     }
+// `
+
+// const schema = makeExecutableSchema({
+//     typeDefs,
+// })
+
+const app = express();
+
+// app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+
+// app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
+
+app.listen(3000, () => {
+    console.log('서버시작');
+})
