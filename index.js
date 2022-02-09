2e6fbd86583bc1b3fb732f2977070a9b0a36b02f
changyun <qkrcksrbs8@naver.com>
2022-02-08 20:06:22 -0800

    graphql, graphiql 주석

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-// import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-// import { makeExecutableSchema } from 'graphql-tools';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
-// const books = [
-//     {title: '자바 메모리구조', author: '김땡땡'}
-//     ,{title: '디자인패턴', author: '박땡땡'}
-//     ,{title: 'v8엔진', author: '오땡땡'}
-// ]
+const books = [
+    {title: '자바 메모리구조', author: '김땡땡'}
+    ,{title: '디자인패턴', author: '박땡땡'}
+    ,{title: 'v8엔진', author: '오땡땡'}
+]
 
-// const typeDefs =`
-//     type Query {
-//         books : [Book]
-//     }
-//     type Book {
-//         title:String
-//         author:String
-//     }
-// `
+const typeDefs =`
+    type Query {
+        books : [Book]
+    }
+    type Book {
+        title:String
+        author:String
+    }
+`
 
-// const schema = makeExecutableSchema({
-//     typeDefs,
-// })
+const schema = makeExecutableSchema({
+    typeDefs,
+})
 
 const app = express();
 
 // app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
-
 // app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
 
 app.listen(3000, () => {
